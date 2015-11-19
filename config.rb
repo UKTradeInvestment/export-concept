ae38cb5bb9bb50ecb369903ae646bcb81b9e92b9
Dom Smith <dom@teneightfive.com>
2015-11-19 19:07:39 +0000

    Add Slim templating language

--- config.rb
+++ config.rb
@@ -16,7 +16,12 @@ page '/*.txt', layout: false
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
-set :layout, 'base'
+Slim::Engine.disable_option_validator!
+Slim::Engine.set_options pretty: true
+Slim::Engine.set_options attr_list_delims: { '(' => ')', '[' => ']' }
+
+# set default layout
+set :layout, 'ukti'
 
 ###
 # Helpers
